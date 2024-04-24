f7c27bafeeac9ed35101fd7611ed38f0b6791e2d
Marcio Borges <desenvolvedormbs@gmail.com>
2024-04-24 11:28:38 -0300

    refactory signin bloc

--- lib/authenticator/signin_bloc.dart
+++ lib/authenticator/signin_bloc.dart
@@ -9,42 +9,40 @@ class SignInBloc extends Bloc<SignInEvent, SignInState> {
   final repository = SignInRepository();
 
   SignInBloc() : super(SignInStateInitial()) {
-    on<SignInEvent>((event, emit) async {
+    on<SignInSetEmailEvent>((event, emit) async {
+      //Validation email could be here
+      emit(SignInStateEmail(null));
+    });
 
-      if(event is SignInSetEmailEvent) {
-        //Validation email could be here
-        emit(SignInStateEmail(null));
-      }
-      if(event is SignInSetPasswordEvent) {
-        //Validation email could be here
-        emit(SignInStatePassword(null));
-      }
+    on<SignInSetPasswordEvent>((event, emit) async {
+      //Validation password could be here
+      emit(SignInStatePassword(null));
+    });
 
+    on<SignInSubmitEvent>((event, emit) async {
       //Validation is be done when form is submitted
-      if (event is SignInSubmitEvent) {
-        if(!Validator.isValidEmail(event.email)){
-          emit(SignInStateEmail("E-mail inválido"));
-        }
-        if(event.password.length < 5){
-          emit(SignInStatePassword("Senha inválida"));
-        }
-        if(Validator.isValidEmail(event.email) && event.password.length > 5) {
-          emit(SignInStateLoading());
-          try {
-            var response = await repository.signIn(event.email, event.password);
-            if((response is Success)) {
-              emit(SignInStateSuccess(response.object.toString()));
-            } else if(response is Unauthorized) {
-              emit(SignInStateField(
-                  SignInStateEmail("Seu e-mail pode estar errado"),
-                  SignInStatePassword("Sua senha pode estar errada")
-              ));
-            } else {
-              emit(SignInStateError((response as Error).object));
-            }
-          } catch(error) {
-            emit(SignInStateError(error));
+
+      if (!Validator.isValidEmail(event.email)) {
+        emit(SignInStateEmail("E-mail inválido"));
+      }
+      if (event.password.length < 5) {
+        emit(SignInStatePassword("Senha inválida"));
+      }
+      if (Validator.isValidEmail(event.email) && event.password.length > 5) {
+        emit(SignInStateLoading());
+        try {
+          var response = await repository.signIn(event.email, event.password);
+          if ((response is Success)) {
+            emit(SignInStateSuccess(response.object.toString()));
+          } else if (response is Unauthorized) {
+            emit(SignInStateField(
+                SignInStateEmail("Seu e-mail pode estar errado"),
+                SignInStatePassword("Sua senha pode estar errada")));
+          } else {
+            emit(SignInStateError((response as Error).object));
           }
+        } catch (error) {
+          emit(SignInStateError(error));
         }
       }
     });
